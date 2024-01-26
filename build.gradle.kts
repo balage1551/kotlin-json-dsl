318f691a92567bfcc7f2994ec083e4e90b6d31ea
balage <vissy.balazs@pmi.co.hu>
2024-01-26 17:16:38 +0100

    Adding commit after release

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,22 @@
+import org.jetbrains.kotlinx.publisher.apache2
+import org.jetbrains.kotlinx.publisher.githubRepo
+import java.nio.file.Files
+
 plugins {
-//    signing
     kotlin("jvm") version "1.9.22"
-    id("hu.vissy.gradle.semanticVersioning") version "0.9"
-//    `maven-publish`
+    id("hu.vissy.gradle.semanticVersioning") version "[1.0.0,2.0.0)"
+    `maven-publish`
+    signing
+    id("org.jetbrains.dokka") version "1.9.10"
+    id("org.jetbrains.kotlin.libs.publisher") version "1.8.10-dev-43"
 }
 
 repositories {
     mavenCentral()
 }
 
-group="hu.vissy"
-version="1.0.0"
+group = "hu.vissy"
+version = "1.0.0"
 
 
 dependencies {
@@ -23,9 +29,10 @@ dependencies {
 java {
     sourceCompatibility = JavaVersion.VERSION_17
     targetCompatibility = JavaVersion.VERSION_17
+    withSourcesJar()
+    withJavadocJar()
 }
 
-//val signingKeyId = project.findProperty("signing.keyId")
 
 semanticVersion {
     releaseTagPrefix = "v"
@@ -33,21 +40,125 @@ semanticVersion {
 }
 
 
-tasks.named("jar") {
-    dependsOn("versionInfo")
+
+publishing {
+    publications {
+        create<MavenPublication>("Maven") {
+            from(components["java"])
+            groupId = "hu.vissy"
+            artifactId = "kotlin-json-dsl"
+            description = "Kotlin DSL wrapper for GSON"
+        }
+        withType<MavenPublication> {
+            pom {
+                packaging = "jar"
+                name.set("kotlin-json-dsl")
+                description.set("Kotlin DSL wrapper for GSON")
+                url.set("https://github.com/balage1551/kotlin-json-dsl/")
+                inceptionYear.set("2024")
+                licenses {
+                    license {
+                        name.set("MIT license")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("Balage1551")
+                        name.set("Balázs Vissy")
+                        email.set("balage42-github@yahoo.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git@github.com:balage1551/kotlin-json-dsl.git")
+                    developerConnection.set("scm:git:ssh:git@github.com:balage1551/kotlin-json-dsl.git")
+                    url.set("https://github.com/balage1551/kotlin-json-dsl")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            url = layout.buildDirectory.dir("staging-deploy").get().asFile.toURI()
+        }
+    }
+}
+
+tasks.jar {
+    dependsOn("setSemanticVersion")
+
+    enabled = true
+    // Remove `plain` postfix from jar file name
+    archiveClassifier.set("")
+
     doLast {
         logger.lifecycle("Building with version: ${project.version}")
     }
 }
 
 
+kotlinPublications {
+    // Default group for all publications
+    defaultGroup.set("hu.vissy")
 
-//publishing {
-//    repositories {
-//        maven {
-//            url = uri(rootDir.resolve("local-plugin-repository"))
-//            name="localPluginRepository"
-//        }
-//    }
-//}
+    // Prefix for artifacts for all publications
+    defaultArtifactIdPrefix.set("kotlin-json-dsl")
 
+    // Set to false if you want to publish empty Javadoc JARs. Maven Central is OK with it
+//    fairDokkaJars.set(false)
+
+    // Signing credentials. You will not be able to publish an artifact to Maven Central without signing
+    signingCredentials(
+        findProperty("signing.keyId").toString(),
+        project.file(project.findProperty("signing.secretKeyFile") as String).readText(),
+        findProperty("signing.password").toString()
+    )
+
+    sonatypeSettings(
+        findProperty("ossrh.username").toString(),
+        findProperty("ossrh.password").toString(),
+        "kotlin-json-dsl"
+    )
+
+    pom {
+        // Use this convenience extension to set up all needed URLs
+        // for the POM in case you're using GitHub
+        githubRepo("balage1551", "kotlin-json-dsl")
+
+        // The year your library was first time published to a public repository
+        inceptionYear.set("2024")
+
+        licenses {
+            // You can contribute extension methods for licences other than Apache 2.0
+            apache2()
+        }
+
+        developers {
+            developer {
+                id = "Balage1551"
+                name = "Balázs Vissy"
+                email = "balage42-github@yahoo.com"
+            }
+        }
+    }
+
+    localRepositories {
+        // Default location for the local repository is build/artifacts/maven/
+        defaultLocalMavenRepository()
+    }
+
+    publication {
+        // By default, publication name is an artifact name. In this case, artifact name
+        // will be kotlin-jupyter-kernel (prefix + name)
+        publicationName.set("core")
+
+        // Description that will appear in POM and on Maven Central search site
+        description.set("Kotlin DSL wrapper of GSON.")
+    }
+}
+
+
+tasks.named("releaseSonatypeStagingRepository") {
+    finalizedBy("commitVersion")
+}
